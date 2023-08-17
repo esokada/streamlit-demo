92c04bf9ce31e47bc1b6c24ced7002bb09306ffe
Elizabeth Okada <zabillet@gmail.com>
2023-08-16 21:24:51 -0700

    added db hookup

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import openai
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 
 st.title("Common Ground demo")
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
 
+
+skey = st.secrets["gcp_service_account"]
+credentials = Credentials.from_service_account_info(
+    skey,
+    scopes=scopes,
+)
+client = gspread.authorize(credentials)
+sh_1 = client.open("common_ground_demo").worksheet("Sheet1")
+sh_2 = client.open("chat_transcripts").worksheet("Sheet1")
+
+
+# TODO: actual nickname system
 def check_password():
     """Returns `True` if the user had the correct password."""
 
@@ -46,8 +64,24 @@ if check_password():
 
     def submit_callback():
         st.session_state.submit = True
+        new_row = [
+            st.session_state.check_1,
+            st.session_state.check_2,
+            st.session_state.check_3,
+            st.session_state.radio_1,
+            st.session_state.free_1,
+        ]
+        sh_1.append_row(new_row, table_range="A1:E1")
 
     if st.session_state.submit:
+        with st.sidebar:
+            if st.button("Submit transcript"):
+                sh_2.append_row([str(st.session_state.messages)], table_range="A1:A1")
+                del st.session_state["messages"]
+            st.write(
+                "This will submit your chat transcript to the researcher and restart your chat"
+            )
+
         if "openai_model" not in st.session_state:
             st.session_state["openai_model"] = "gpt-3.5-turbo"
 
@@ -80,25 +114,31 @@ if check_password():
             st.session_state.messages.append(
                 {"role": "assistant", "content": full_response}
             )
+
     else:
         if st.session_state.consent:
             if not st.session_state.submit:
                 with st.form("demo_survey"):
                     st.header("Demographic survey")
-                    st.checkbox("Checkbox question 1")
-                    st.checkbox("Checkbox question 2")
-                    st.checkbox("Checkbox question 3")
-                    st.radio("Radio question", ("Option 1", "Option 2", "Option 3"))
-                    st.text_input("Free text question")
+                    check_1 = st.checkbox("Checkbox question 1", key="check_1")
+                    check_2 = st.checkbox("Checkbox question 2", key="check_2")
+                    check_3 = st.checkbox("Checkbox question 3", key="check_3")
+                    radio_1 = st.radio(
+                        "Radio question",
+                        ("Option 1", "Option 2", "Option 3"),
+                        key="radio_1",
+                    )
+                    free_1 = st.text_input("Free text question", key="free_1")
                     submit = st.form_submit_button(
-                        "Submit demographic survey", on_click=submit_callback
+                        "Submit demographic survey",
+                        on_click=submit_callback,
                     )
 
         else:
             with st.form("Consent form"):
                 st.header("Consent form")
                 st.write("Consent text")
-                st.checkbox("I agree [...]")
+                consent_check = st.checkbox("I agree [...]")
                 consent = st.form_submit_button(
                     "Submit consent form", on_click=consent_callback
                 )
